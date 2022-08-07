b85499d8c116a07ddca09b51160f83f8d591ba00
Lê Hàn Minh Khang <khanghugo123@gmail.com>
2022-08-07 09:23:28 -0400

    Create generics.nix

---
+++ jambonz/generics.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "node";
+    buildInputs = [
+        nodejs
+        nodePackages.npm
+        yarn
+        docker
+        docker-compose
+        python39
+        gtk2
+        gtk3
+        libnotify
+        pkgconf
+        nss
+        xvfb-run
+        xorg.xauth
+    ];
+    # apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+    shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+        export CYPRESS_INSTALL_BINARY=0
+        export CYPRESS_RUN_BINARY=${pkgs.cypress}/bin/Cypress
+    '';
+}
